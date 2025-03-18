e3e1f6e50712bf4f41fe182f099aaccec7f6c70a
MohamedKadi <gamesmohamed14@gmail.com>
2025-03-18 14:39:44 +0000

    validation handling in errorController

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,4 +1,12 @@
 module.exports = (err, req, res, next) => {
+  if (err.code === 11000) {
+    err.statusCode = 400;
+    err.status = 'fail';
+    err.message = `Duplicate field value: ${Object.keys(err.keyValue).join(
+      ', '
+    )}. Please use another value`;
+  } // handle duplicate key error
+
   res.status(err.statusCode || 500).json({
     status: err.status || 'something went wrong',
     message: err.message,
--- Lib/validators/userValidator/userValidator.js
+++ Lib/validators/userValidator/userValidator.js
@@ -2,11 +2,13 @@ const { checkSchema } = require('express-validator');
 
 const userValidationSchema = checkSchema({
   username: {
+    trim: true,
     isString: true,
     notEmpty: true,
     errorMessage: 'Username must be a non-empty string',
   },
   email: {
+    trim: true,
     isEmail: true,
     errorMessage: 'Invalid email address',
   },
